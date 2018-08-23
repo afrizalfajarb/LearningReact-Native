9c5addb13de35cf6fb07ee2d70be8cdc593e01dc
AfrizalFajarB <afrizalfajar14@gmail.com>
2018-08-23 15:25:00 +0700

    langkah kedua

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Nama : Afrizal Fajar Barokah</Text>
-        <Text>Kelas : XI RPL 4</Text>
-        <Text>Absen : 03</Text>
+    render() {
+        return (
+          <View style={styles.container}>
+            <Text>Nama : Afrizal Fajar Barokah</Text>
+            <Text>Kelas : XI RPL 4</Text>
+            <Text>Absen : 03</Text>
+            <Image
+    source={require('./afrizal.jpg')}
+    style={ { height: 576 , width:876}}/>
+
+               
+           
       </View>
     );
   }
